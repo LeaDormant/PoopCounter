cbed4b1d62f27b03a34898e081d7cb199a6e4eae
kaza4ello <isaev@ratio.digital>
2023-02-18 17:07:37 +0200

    Initial

---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="modal" @click="$emit('close')">
+    <div class="inner">
+      <div class="message" v-if="success">
+        <img src="@/assets/img/me.svg" alt="lovely">
+        <p>Congratulation! You've pooped!</p>
+      </div>
+      <div class="message" v-else>
+        <img src="@/assets/img/sorry.svg" alt="">
+        <p>We're very that you didn't poop...</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['success']
+}
+</script>
+
+<style scoped>
+img {
+  height: 50px;
+  width: 50px;
+}
+.message {
+  display: flex;
+  grid-gap: 10px;
+  align-items: center;
+}
+.modal {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.517);
+}
+.inner {
+  background-color: white;
+  border-radius: 20px;
+  padding: 10px;
+  box-shadow: 0 0 20px 0px #0000004a;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <header>
+      <p>PoopCounter</p>
+    </header>
+    <div class="content">
+      <div class="left">
+        <p>Did you poop?</p>
+        <div class="answers">
+          <div class="yes" @click="addPoop">
+            <img src="@/assets/img/poop.svg" alt="poop">
+            <p>Yes!</p>
+          </div>
+          <div class="no" @click="noPoop">
+            <img src="@/assets/img/no.svg" alt="nopoop">
+            <p>No...</p>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <p>Your poop count is...</p>
+        <div class="count">
+          <p>{{ count }}</p>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <p class="names">made by Lina and Danya</p>
+      <div class="emails">
+        <a href = "mailto:linatolmach@gmail.com">linatolmach@gmail.com</a>
+        <a href = "mailto:ddanili56@gmail.com">ddanili56@gmail.com</a>
+      </div>
+    </footer>
+    <Modal v-if="showModal" @close="showModal = false" :success="success"></Modal>
+  </div>
+</template>
+
+<script>
+  import Modal from '@/components/Modal.vue'
+ export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      count: 0,
+      showModal: false,
+      success: true
+    }
+  },
+  methods: {
+    addPoop() {
+      this.count++;
+      localStorage.setItem('count', this.count.toString());
+      this.showModal = true;
+      this.success = true;
+    },
+    noPoop() {
+      this.showModal = true;
+      this.success = false;
+    }
+  },
+  mounted() {
+    this.count = localStorage.getItem('count');
+  }
+ }
+</script>
